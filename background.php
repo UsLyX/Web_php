ac25c0504121c3f2f3657f92c99dfe0b2444fab6
UsLyX <109165881+UsLyX@users.noreply.github.com>
2023-11-20 09:19:31 +0300

    add functions from lr5

---
+++ background.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'connect.php';
+session_start();
+$background = $_POST['background'];
+$nickname = $_SESSION['nickname'];
+echo $nickname;
+
+$conn->query("UPDATE `person` SET `background` = '$background' WHERE `person`.`nickname` = '$nickname'");
+
+$_SESSION['background'] = $conn->query("SELECT `background` FROM `person` WHERE `nickname` = '$nickname'");
+$_SESSION['background'] = $_SESSION['background']->fetch_assoc();
+print_r($_SESSION['background']);
+
+$conn->close();
+//Header('Location: /web_php/index.php');
+?>
+
